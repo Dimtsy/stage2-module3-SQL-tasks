dc2daf7200e45bdc50beb02393ed1ebc5b07e258
Dzmitry Tsybulski <dzmitrytsybulski@gmail.com>
2023-07-24 21:45:43 +0300

    Correction2 Module #3. Task #1. SQL

--- lesson5/src/main/resources/SOLUTION.sql
+++ lesson5/src/main/resources/SOLUTION.sql
@@ -1,7 +1,7 @@
 SELECT * FROM payment WHERE amount >= 500;
 SELECT * FROM student WHERE birthday < DATEADD(year, -20, '20230707');
 SELECT * FROM student WHERE birthday > DATEADD(year, -20, '20230707') AND groupnumber = 10;
-SELECT * FROM student WHERE NAME = 'Mike' OR groupnumber = 4 OR groupnumber = 5 OR groupnumber = 6;
+SELECT * FROM student WHERE NAME LIKE 'Mike%' OR groupnumber IN (4, 5, 6);
 SELECT * FROM payment WHERE payment_date > DATEADD(month, -8, '20230707');
 SELECT * FROM student WHERE name LIKE 'A%';
 SELECT * FROM student WHERE birthday < (yy, -20, GETDATE());
