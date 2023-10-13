6d338bf34c05c32e25f589e75e80265af44eaabb
Dzmitry Tsybulski <dzmitrytsybulski@gmail.com>
2023-10-13 23:37:50 +0300

    Create Module #3. Task #1. SQL

--- lesson5/src/main/resources/SOLUTION.sql
+++ lesson5/src/main/resources/SOLUTION.sql
@@ -1,7 +1,7 @@
 SELECT * FROM payment WHERE amount >= 500;
-SELECT * FROM student WHERE birthday < DATEADD(year, -20, '20230707');
-SELECT * FROM student WHERE birthday > DATEADD(year, -20, '20230707') AND groupnumber = 10;
+SELECT * FROM student WHERE birthday < DATEADD(year, -20, '20230101');
+SELECT * FROM student WHERE birthday > DATEADD(year, -20, '20230101') AND groupnumber = 10;
 SELECT * FROM student WHERE NAME LIKE 'Mike%' OR groupnumber IN (4, 5, 6);
-SELECT * FROM Payment WHERE payment_date > DATEADD(MONTH, -8, CURRENT_DATE());
+SELECT * FROM Payment WHERE payment_date > DATEADD(MONTH, -8, '20230101');
 SELECT * FROM student WHERE name LIKE 'A%';
 SELECT * FROM student WHERE (NAME LIKE 'Roxi%' AND groupnumber = 4) OR (NAME LIKE 'Tallie%' AND groupnumber = 9);
