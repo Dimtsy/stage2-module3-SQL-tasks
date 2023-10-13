ecb5c12848de3c055f858d906597f4cedc13b3dc
Dzmitry Tsybulski <dzmitrytsybulski@gmail.com>
2023-10-13 18:47:42 +0300

    Correction7 Module #3. Task #1. SQL

--- lesson12/src/main/resources/SOLUTION.sql
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -1,4 +1,4 @@
-DELETE FROM student WHERE id IN (SELECT student_id FROM mark GROUP BY student_id HAVING MIN(mark) > 4);
+DELETE FROM Student WHERE id IN (SELECT student_id FROM Mark WHERE subject_id IN (SELECT sub.id FROM Subject sub WHERE grade >= 4));
 DELETE FROM Student WHERE id IN (SELECT DISTINCT student_id FROM Mark WHERE mark < 4);
 DELETE FROM PaymentType WHERE name = 'DAILY';
 DELETE FROM Mark WHERE mark < 7;
